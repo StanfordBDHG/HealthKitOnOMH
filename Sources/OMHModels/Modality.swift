c38b3e3330685f367bc0a9a5f39983f859ecef31
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-12-11 17:33:22 -0500

    Add Modality type

---
+++ Sources/OMHModels/Modality.swift
@@ -0,0 +1,12 @@
+//
+// This source file is part of the HealthKitOnOMH open source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+
+
+public enum Modality: String, Codable {
+    case sensed = "sensed"
+    case selfReported = "self-reported"
+}
