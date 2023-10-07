37ba58c777e0bbe7a6d3fa5bc6c9e1fe29be6455
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-10-07 10:41:31 -0400

    Fix swiftlint errors

--- Sources/OMHModels/TimeInterval.swift
+++ Sources/OMHModels/TimeInterval.swift
@@ -10,7 +10,7 @@ import Foundation
 
 /// This schema describes an interval of time.
 /// Generated from Open mHealth `omh:time-interval:1.0` (https://www.openmhealth.org/documentation/#/schema-docs/schema-library/schemas/omh_time-interval)
-public struct TimeInterval: Codable, Equatable, Comparable {
+public struct TimeInterval: Codable, Equatable {
     /// The start timestamp of this interval
     public var startDateTime: Date
 
@@ -26,14 +26,7 @@ public struct TimeInterval: Codable, Equatable, Comparable {
         self.endDateTime = endDateTime
     }
     
-    public static func ==(lhs: TimeInterval, rhs: TimeInterval) -> Bool {
-        return lhs.startDateTime == rhs.startDateTime && lhs.endDateTime == rhs.endDateTime
-    }
-    
-    public static func <(lhs: TimeInterval, rhs: TimeInterval) -> Bool {
-        if lhs.startDateTime != rhs.startDateTime {
-            return lhs.startDateTime < rhs.startDateTime
-        }
-        return lhs.endDateTime < rhs.endDateTime
+    public static func == (lhs: TimeInterval, rhs: TimeInterval) -> Bool {
+        lhs.startDateTime == rhs.startDateTime && lhs.endDateTime == rhs.endDateTime
     }
 }
