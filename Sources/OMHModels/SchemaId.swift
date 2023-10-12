a4ff60535cf0c21a964d2b01ebca57818ba0c856
Vishnu Ravi <vishnur@stanford.edu>
2023-10-12 18:57:56 -0400

    Update SchemaId and TypedUnitValue

--- Sources/OMHModels/SchemaId.swift
+++ Sources/OMHModels/SchemaId.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 
-/// A schema identifier for an Open mHealth schema
-/// Generated from Open mHealth `omh:schema-id:1.1` (https://www.openmhealth.org/documentation/#/schema-docs/schema-library/schemas/omh_schema-id)
-public struct SchemaId: Sendable, Codable, CustomStringConvertible {
+/// A schema identifier
+/// Generated from IEEE 1752.1`schema-id-1.0` (https://w3id.org/ieee/ieee-1752-schema/schema-id.json)
+public struct SchemaId: Sendable, Codable {
     /// The namespace of the schema. A namespace serves to disambiguate schemas with conflicting names.
     public var namespace: SchemaNamespace
 
@@ -19,14 +19,6 @@ public struct SchemaId: Sendable, Codable, CustomStringConvertible {
 
     /// The version of the schema.
     public var version: String
-
-    /// A URL to retrieve the schema. If a URL is not specified, it is assumed that the schema can be located using other means.
-    public var url: URL?
-    
-    
-    public var description: String {
-        "\(namespace.rawValue):\(name):\(version))"
-    }
     
     
     public init (
--- Sources/OMHModels/TypedUnitValue.swift
+++ Sources/OMHModels/TypedUnitValue.swift
@@ -11,6 +11,8 @@ import Foundation
 public protocol UnitProtocol: Codable, Sendable {
 }
 
+/// A numerical value with a unit of measure
+/// Generated from IEEE 1752.1 `unit-value-1.0` (https://w3id.org/ieee/ieee-1752-schema/unit-value.json)
 public struct TypedUnitValue<T: UnitProtocol>: Codable, Sendable {
     private enum CodingKeys: String, CodingKey {
         case unit, value
