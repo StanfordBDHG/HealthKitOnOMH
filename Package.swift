4f8021875490ffaf91244d4c21699bde322402cb
Andreas Bauer <andreas.bauer@stanford.edu>
2024-10-28 22:45:34 +0100

    Enable strict concurrency checking (#13)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:6.0
 
 //
 // This source file is part of the HealthKitOnOMH open source project
@@ -8,6 +8,7 @@
 // SPDX-License-Identifier: MIT
 //
 
+import class Foundation.ProcessInfo
 import PackageDescription
 
 
@@ -21,19 +22,39 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/StanfordBDHG/OMHModels.git", .upToNextMajor(from: "0.1.0"))
-    ],
+    ] + swiftLintPackage(),
     targets: [
         .target(
             name: "HealthKitOnOMH",
             dependencies: [
                 .product(name: "OMHModels", package: "OMHModels")
-            ]
+            ],
+            plugins: [] + swiftLintPlugin()
         ),
         .testTarget(
             name: "HealthKitOnOMHTests",
             dependencies: [
                 .target(name: "HealthKitOnOMH")
-            ]
+            ],
+            plugins: [] + swiftLintPlugin()
         )
     ]
 )
+
+
+func swiftLintPlugin() -> [Target.PluginUsage] {
+    // Fully quit Xcode and open again with `open --env SPEZI_DEVELOPMENT_SWIFTLINT /Applications/Xcode.app`
+    if ProcessInfo.processInfo.environment["SPEZI_DEVELOPMENT_SWIFTLINT"] != nil {
+        [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
+    } else {
+        []
+    }
+}
+
+func swiftLintPackage() -> [PackageDescription.Package.Dependency] {
+    if ProcessInfo.processInfo.environment["SPEZI_DEVELOPMENT_SWIFTLINT"] != nil {
+        [.package(url: "https://github.com/realm/SwiftLint.git", from: "0.55.1")]
+    } else {
+        []
+    }
+}
